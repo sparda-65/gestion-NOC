bb830685d098f9cd08f71d392b65ee2edcc119f7
sparda-65 <chahboune.abdellah@gmail.com>
2018-11-12 21:44:47 +0100

    ajout du model superviseur avec les attributs

---
+++ api/models/Superviseur.js
@@ -0,0 +1,69 @@
+/**
+ * Superviseur.js
+ *
+ * @description :: A model definition.  Represents a database table/collection/etc.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    nom:{
+      type:'string',
+      required:true
+    },
+
+    prenom:{
+      type:'string',
+      required:true
+    },
+
+    dateEntry:{
+      type:'string',
+      columnType:'date',
+      required:true
+    },
+
+    equipe:{
+      type:'string',
+    },
+
+    access:[{
+      m200: {type:'boolean'},
+      vpn:{type:'boolean'}
+    }],
+
+    ressources:[{
+      pcPortable:{type:'boolean'},
+      telephone:{type:'boolean'}
+    }],
+    dateFin:{
+      type:'string',
+      columnType:'date'
+    },
+
+    actif:{
+      type:'boolean'
+    },
+
+    chart:{
+      type:'boolean'
+    }
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+};
+
