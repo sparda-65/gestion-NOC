81740a0224afeb0231a58113a6e5c8758b07d31f
sparda-65 <chahboune.abdellah@gmail.com>
2019-03-02 22:30:33 +0100

    ajouter la suppression photo sur  destroy one superviseur dans backend

--- api/controllers/superviseurs/destroy-one-superviseur.js
+++ api/controllers/superviseurs/destroy-one-superviseur.js
@@ -23,7 +23,21 @@ module.exports = {
 
   fn: async function (inputs, exits) {
 
+    var sup =await Superviseur.findOne({
+      id:inputs.id
+    });
     await Superviseur.destroy({id:inputs.id});
+
+    sails.log(sup.imageUploadFd);
+
+    var fs = require('fs');
+
+    var filePath = sup.imageUploadFd;
+
+    fs.unlink(filePath, (err) => {
+      if (err) {throw err;}
+      console.log(filePath+' was deleted');
+    });
 
     return exits.success();
 
